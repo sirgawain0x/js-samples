110a93b74fdcc87eec2fa09c6f63e5361e8c9948
Alexia Dawson <adawson@fullsail.com>
2012-05-05 10:46:37 -0400

    Creating sample files for adding an Edit link

---
+++ js/json.js
@@ -0,0 +1,60 @@
+{
+    "items":
+    [
+        {
+            "category"   :["Category:", "Poison"],
+            "subcategory":["Subcategory:", "ingested"],
+            "iname"      :["Item Name:", "lily of the valley"],
+            "quantity"   :["Quantity:", "1"],
+            "weight"     :["Weight:", "0.1 oz."],
+            "conceal"    :["Concealable:", "yes"],
+            "favorites"  :["Favorites:", "no"],
+            "notes"      :["Notes:", "Sweet red berries, Walter White's favorite"],
+            "saved"      :["Entered on:", "2011-10-11T15:29:23.710Z"]
+        },
+        {
+            "category"   :["Category:", "Poison"],
+            "subcategory":["Subcategory:", "ingested"],
+            "iname"      :["Item Name:", "iocaine powder"],
+            "quantity"   :["Quantity:", "1"],
+            "weight"     :["Weight:", "0.1 oz."],
+            "conceal"    :["Concealable:", "yes"],
+            "favorites"  :["Favorites:", "yes"],
+            "notes"      :["Notes:", "Odorless, tasteless, dissolves instantly in water."],
+            "saved"      :["Entered on:", "2011-10-09T16:57:59.098Z"]
+        },
+        {
+            "category"   :["Category:", "Bladed"],
+            "subcategory":["Subcategory:", "sword"],
+            "iname"      :["Item Name:", "katana"],
+            "quantity"   :["Quantity:", "2"],
+            "weight"     :["Weight:", "1.4 lbs."],
+            "conceal"    :["Concealable:", "no"],
+            "favorites"  :["Favorites:", "no"],
+            "notes"      :["Notes:", "Stick them with the pointy end!"],
+            "saved"      :["Entered on:", "2011-10-02T15:25:56.983Z"]
+        },
+        {
+            "category"   :["Category:", "Thrown"],
+            "subcategory":["Subcategory:", "ninja star"],
+            "iname"      :["Item Name:", "shuriken"],
+            "quantity"   :["Quantity:", "1"],
+            "weight"     :["Weight:", "0.3 oz."],
+            "conceal"    :["Concealable:", "yes"],
+            "favorites"  :["Favorites:", "yes"],
+            "notes"      :["Notes:", "Will stick in eyes"],
+            "saved"      :["Entered on:", "2010-05-11T15:46:22.899Z"]
+        },
+        {
+            "category"   :["Category: ", "Blunt"],
+            "subcategory":["Subcategory: ", ""],
+            "iname"      :["Item Name: ", "brass knuckles"],
+            "quantity"   :["Quantity: ", 6],
+            "conceal"    :["Concealable: ", "no"],
+            "favorites"  :["Favorites: ", "yes"],
+            "weight"     :["Weight: ", "0.4 lbs."],
+            "notes"      :["Notes: ", "Good for smashing faces."],
+            "saved"      :["Entered on: ", "2011-10-06T18:01:02.462Z"]
+        }
+    ]
+}
---
+++ js/read.js
@@ -0,0 +1,74 @@
+// Your name
+// Project description
+
+$('#one').live('pageinit', function () {
+    console.log("Page one loaded."); // only runs on the first load unless the page is refreshed.
+
+    // Step 1: target our list and empty it http://api.jquery.com/empty/
+    $("ul").empty();
+
+    // Step 2: Grab list item details from index.html
+    $('<li data-role="list-divider">Friday, October 8, 2010 <span class="ui-li-count">3</span></li>' +
+        '<li><a href="index.html"><h3>Stephen Weber</h3>' +
+        '<p><strong>You have been invited to a meeting at Filament Group in Boston, MA</strong></p>' +
+        '<p>Hey Stephen, we have got a meeting with the jQuery team.</p>' +
+        '<p class="ui-li-aside"><strong>6:24</strong>PM</p></a></li>').appendTo("ul");
+
+    // Step 3: refresh the listview
+    $("ul").listview('refresh');
+
+
+    // Get localStorage
+    $("#storage").on('click', function(){
+        $("#itemList").empty();
+        for (var i= 0, j=localStorage.length; i<j ; i++){
+            var key = localStorage.key(i);
+            var item = JSON.parse(localStorage.getItem(key));
+            console.log(item);
+            var makeSubList = $("<li></li>");
+            var makeSubLi = $( "<h3>"+item.iname[1]+"</h3>"+
+                "<p><strong>"+item.category[1]+"</strong></p>"+
+                "<p>"+item.quantity[1]+"</p>" +
+                "<p>"+item.notes[1]+"</p>" );
+            var makeLink = $("<a href='#' id='"+key+"'>Edit</a>");
+            makeLink.on('click', function(){
+                console.log("This is my key: "+this.id);
+            });
+            //makeSubList.append(makeSubLi).append(makeLink).appendTo("#itemList");
+            makeLink.html(makeSubLi);
+            makeSubList.append(makeLink).appendTo("#itemList");
+
+            //$("#itemList").trigger('create');
+        }; // end for loop
+        $("ul").listview('refresh');
+    });  // end storage.on
+
+}); // end #one pageinit
+
+
+
+/*
+ // transition to page, using data from a form and reloading the page from scratch
+ var toChangePage = function (toPageId) {
+ $.mobile.changePage("#" + toPageId , {
+ type:"post",
+ data:$("form").serialize(),
+ reloadPage:true
+ });
+ };
+ */
+$('#two').live('pageinit', function () {
+    console.log("Page two loaded.");
+}); // end #two pageinit
+
+$('#popup').live('pageinit', function () {
+    console.log("Popup page loaded.");
+    /*
+     // finds a link that has href="#one" and overrides the link action
+     $("a[href='#one']").on('click', function(e){  // .on is defined in jQ 1.7.1
+     e.preventDefault();
+     console.log("Moving to PAGE ONE with the toChangePage function");
+     toChangePage("one");
+     });
+     */
+});  // end #popup pageinit
